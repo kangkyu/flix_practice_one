601fb7c44f7148d5ed2955a221e2c6b1d7cb12e6
kangkyu <kangkyu1111@gmail.com>
2014-11-16 00:05:49 -0800

    add login feature by session resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   resources :users
   get 'signup', to: 'users#new'
 
-  resources :session
-  
+  resource :session
+  get 'signin', to: 'sessions#new'
+
   root "movies#index"
   resources :movies do
     resources :reviews
--- spec/support/attributes.rb
+++ spec/support/attributes.rb
@@ -24,6 +24,7 @@ end
 def user_attributes(overrides = {})
   {
     name: "Example User",
+    username: "userexample",
     email: "user@example.com",
     password: "secret",
     password_confirmation: "secret"
